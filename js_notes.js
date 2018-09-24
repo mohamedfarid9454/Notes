b0e6d57718c64e476abf4559486dd2d05bbace4c
Mohamed Abdullah <mohamedfarid648@gmail.com>
2018-09-24 11:37:53 +0200

    Create js_notes.js

---
+++ js_notes.js
@@ -0,0 +1,8 @@
+1. show-loading-image-while-ajax-is-performed
+
+https://stackoverflow.com/questions/4684722/show-loading-image-while-ajax-is-performed
+
+
+
+2. FancyGrid(dynamic tables):
+https://fancygrid.com/samples/columns/order-column
