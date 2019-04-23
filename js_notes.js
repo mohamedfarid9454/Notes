018ef5298c718ee25234c023d9834a270aa1fabc
Mohamed Abdullah <mohamedfarid648@gmail.com>
2019-04-23 15:14:42 +0200

    Sum elements in array(ts)

--- js_notes.js
+++ js_notes.js
@@ -10,3 +10,7 @@ https://fancygrid.com/samples/columns/order-column
 
 3.Map in TS
 https://www.learnrxjs.io/operators/transformation/map.html
+
+4.Sum elements in array(ts)
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+
